820a9d6077e345d897d550d368e11431d99ef5b4
Latief <latifftrahman@gmail.com>
2026-08-16 12:09:31 +0700

    perubahan ikon

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,15 @@ import "./main.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./pages/auth/AuthContext.jsx";
 
+import hero from './assets/hero.png';
+
 axios.defaults.baseURL = "https://presensi-magang-backend.vercel.app/api";
+
+const link = document.createElement("link");
+link.rel = "icon";
+link.type = "image/png";
+link.href = logoputih;
+document.head.appendChild(link);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
