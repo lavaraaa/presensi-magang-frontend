958fff94e70a93b91d070e52dd1d54afd363de42
Latief <latifftrahman@gmail.com>
2026-08-16 12:24:08 +0700

    perbaikan offcanvas

--- src/main.jsx
+++ src/main.jsx
@@ -10,15 +10,7 @@ import "./main.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./pages/auth/AuthContext.jsx";
 
-import hero from './assets/hero.png';
-
 axios.defaults.baseURL = "https://presensi-magang-backend.vercel.app/api";
-
-const link = document.createElement("link");
-link.rel = "icon";
-link.type = "image/png";
-link.href = hero;
-document.head.appendChild(link);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
